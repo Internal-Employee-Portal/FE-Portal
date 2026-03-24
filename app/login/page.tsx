1c73132ac5a1e9ec3cf1729708691b3aa5fd3f77
youmin29 <babybana0829@naver.com>
2026-03-24 18:22:14 +0900

    ✨ Feat: 로그인 화면 추가

---
+++ app/login/page.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      console.log("asdf");
+      const res = await fetch("http://localhost:8001/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+
+      if (!res.ok) {
+        throw new Error("Login failed");
+      }
+
+      const data = await res.json();
+
+      // 토큰 저장
+      localStorage.setItem("token", data.access_token);
+
+      // 이동
+      router.push("/employees");
+    } catch (err) {
+      setError("이메일 또는 비밀번호가 올바르지 않습니다.");
+    }
+  };
+
+  return (
+    <div className="container d-flex justify-content-center align-items-center vh-100">
+      <div className="card p-4 shadow" style={{ width: "400px" }}>
+        <h3 className="text-center mb-4">로그인</h3>
+
+        {error && <div className="alert alert-danger">{error}</div>}
+
+        <form onSubmit={handleLogin}>
+          <div className="mb-3">
+            <label className="form-label">이메일</label>
+            <input
+              type="email"
+              className="form-control"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label">비밀번호</label>
+            <input
+              type="password"
+              className="form-control"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary w-100">
+            로그인
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
