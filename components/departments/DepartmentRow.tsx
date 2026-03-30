ec3edcfb74052ff413862ae04cc932c1c89c3ef3
youmin29 <babybana0829@naver.com>
2026-03-30 23:10:31 +0900

    🔨 Refactor: 부서 직원 수 리팩토링

--- components/departments/DepartmentRow.tsx
+++ components/departments/DepartmentRow.tsx
@@ -6,7 +6,7 @@ export default function DepartmentRow({ dept, onClick }: any) {
       <td>{dept.name}</td>
       <td>{dept.description}</td>
       <td>{dept.manager_full_name ?? "-"}</td>
-      <td>{dept.employeeCount ?? 0}</td>
+      <td>{dept.employee_count ?? 0}</td>
       <td>{dept.created_at?.split("T")[0]}</td>
     </tr>
   );
